33cad6e5488c1ec23d105cd280b6bf68d839c470
Niklas Hambüchen <mail@nh2.me>
2013-01-28 05:26:21 +0100

    Some cleanup

--- Browse.hs
+++ Browse.hs
@@ -12,8 +12,6 @@ import PprTyThing
 import Pretty (showDocWith, Mode(OneLineMode))
 import qualified Gap
 
-import Debug.Trace
-
 ----------------------------------------------------------------
 
 browseModule :: Options -> String -> IO String
@@ -33,51 +31,21 @@ browse :: Options -> String -> IO [String]
 browse opt mdlName = withGHC $ do
     _ <- initSession0 opt
     modinfo <- lookupModuleInfo
-    -- getInfo :: GhcMonad m => Name -> m (Maybe (TyThing, Fixity, [Instance]))
-    case modinfo of
-      Nothing -> error "niklas"
-      Just mi -> do let exports = modInfoExports mi
-                        tythings = modInfoTyThings mi
-                        toplevel = modInfoTopLevelScope mi
-                    -- trace ("   " ++ showOutputable exports) $ return ()
-                    bla <- getInfo (exports !! 20)
-                    t <- exprType "(\\x -> x)"
-                    -- Useful: idType :: Id -> Kind
-                    -- trace ("xx " ++ showOutputable t) $ return ()
-                    -- trace ("yy " ++ showOutputable bla) $ return ()
-                    -- trace ("tythings " ++ showOutputable tythings) $ return ()
-                    -- trace ("toplevel " ++ showOutputable toplevel) $ return ()
-                    -- trace ("lasttything " ++ showOutputable lasttything) $ return ()
-                    return ()
-    -- return $ maybeNamesToStrings modinfo
-    case modinfo of
-      Nothing -> return []
-      Just mi -> do
-        case modInfoTopLevelScope mi of
-          Nothing -> return []
-          Just toplevel -> mapM (getNameAndType mi) toplevel
+    maybe (return []) getNamesWithTypes modinfo
   where
-    render name typ = getOccString name ++ " :: " ++ pretty typ
-    getNameAndType modinfo name = do
+    getNamesWithTypes modinfo = maybe (return []) (mapM (getNameWithType modinfo)) $ modInfoTopLevelScope modinfo
+
+    getNameWithType modinfo name = do
       tyThing <- modInfoLookupName modinfo name
       return $ case tyThing of
-        Just (AnId i) -> render name (idType i)
+        Just (AnId i)            -> render name (idType i)
         Just (ADataCon _datacon) -> "ADataCon " ++ render name (dataConUserType _datacon)
         Just (ATyCon _tycon)     -> "ATyCon " ++ getOccString name -- render name (synTyConType _tycon)
         Just (ACoAxiom _coaxiom) -> "ACoAxiom " ++ getOccString name
-        Nothing -> "no tyThing " ++ getOccString name
+        Nothing                  -> "no tyThing " ++ getOccString name
 
-    -- lookupModuleInfo = findModule (mkModuleName mdlName) Nothing >>= getModuleInfo
-    modm = findModule (mkModuleName mdlName) Nothing
-    lookupModuleInfo = modm >>= getModuleInfo
-    maybeNamesToStrings = maybe [] (map getOccString . modInfoExports)
-    -- maybeNamesToStrings = maybe [] (\x -> trace (" asdf" ++ pmodinfo x) $ map getOccString (modInfoExports x))
-
-showOutputable :: (Outputable a) => a -> String
-showOutputable = showSDoc . ppr
-
-pmodinfo :: ModuleInfo -> String
-pmodinfo mod = let Just modiface = modInfoIface mod in showOutputable (mi_exports modiface)
+    lookupModuleInfo = findModule (mkModuleName mdlName) Nothing >>= getModuleInfo
+    render name typ = getOccString name ++ " :: " ++ pretty typ
 
 pretty :: Type -> String
 pretty = showDocWith OneLineMode . Gap.styleDoc (mkUserStyle neverQualify AllTheWay) . pprTypeForUser False
